9ac1af82a1706d3669bd16c97e3173cbfd9a9f56
Paweł Borkowski <borekosp@gmail.com>
2020-01-23 22:13:23 +0100

    working on newPayment

--- src/app/bookmarks-peako/payment/new-payment/new-payment.component.ts
+++ src/app/bookmarks-peako/payment/new-payment/new-payment.component.ts
@@ -1,5 +1,5 @@
-import { Component, OnInit } from '@angular/core';
-import { FormGroup, FormBuilder, FormControl, Validators, AbstractControl } from '@angular/forms';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { FormGroup, FormBuilder, FormControl, Validators, AbstractControl, NgForm } from '@angular/forms';
 import * as IBAN from 'iban';
 @Component({
     selector: 'app-new-payment',
@@ -7,6 +7,7 @@ import * as IBAN from 'iban';
     styleUrls: ['./new-payment.component.scss']
 })
 export class NewPaymentComponent implements OnInit {
+    @ViewChild('form', { static: true }) form: NgForm;
     newPaymentForm: FormGroup;
     constructor(private fb: FormBuilder) {
         this.createForm();
@@ -16,8 +17,9 @@ export class NewPaymentComponent implements OnInit {
 
     createForm() {
         this.newPaymentForm = new FormGroup({
-            accNumber: new FormControl(null, [Validators.required], this.ValidateIban),
+            accNumber: new FormControl(null, [Validators.required, this.ValidateIban]),
             amount: new FormControl(null, [Validators.required]),
+            recipientsAddress: new FormControl(null),
             title: new FormControl(null, [Validators.required]),
             nameOrder: new FormControl(null, [Validators.required])
         });
@@ -29,5 +31,9 @@ export class NewPaymentComponent implements OnInit {
             return null;
         }
         return { accountNumber: true };
+    }
+
+    reactiveSubmit() {
+        console.log(this.newPaymentForm);
     }
 }
