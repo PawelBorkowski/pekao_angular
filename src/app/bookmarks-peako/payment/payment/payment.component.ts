241cc3fcf0d7bf0e77af6a806530f76b51f6e165
Paweł Borkowski <borekosp@gmail.com>
2020-02-04 17:40:45 +0100

    add function pokaz wiecej- finished, continuing work on app

--- src/app/bookmarks-peako/payment/payment/payment.component.ts
+++ src/app/bookmarks-peako/payment/payment/payment.component.ts
@@ -4,47 +4,51 @@ import { AuthService } from 'src/app/login/auth.service';
 import { SendingFormsService } from '../sending-forms.service';
 
 @Component({
-  selector: 'app-payment',
-  templateUrl: './payment.component.html',
-  styleUrls: ['./payment.component.scss']
+    selector: 'app-payment',
+    templateUrl: './payment.component.html',
+    styleUrls: ['./payment.component.scss']
 })
 export class PaymentComponent implements OnInit {
-  accounts: IAccount[];
+    accounts: IAccount[];
 
-  amountCalculated;
-  objectAmount: object;
-  amountMY = 0;
-  status = false;
-  id: any;
-  constructor(public authService: AuthService, private myService: SendingFormsService) { }
+    amountCalculated;
+    objectAmount: object;
+    amountMY = 0;
+    status = false;
+    id: any;
+    end: number = 5;
+    start: number = 0;
+    constructor(public authService: AuthService, private myService: SendingFormsService) {}
 
-  ngOnInit() {
-    this.authService.profile$.subscribe(profile => {
-      if (!profile) {
-        return;
-      }
-      this.accounts = Object.values(profile.accounts);
+    ngOnInit() {
+        this.authService.profile$.subscribe(profile => {
+            if (!profile) {
+                return;
+            }
+            this.accounts = Object.values(profile.accounts);
 
-      let total = 0;
-      this.objectAmount = Object.values(profile.accounts['ID-1'].transactions);
+            let total = 0;
+            this.objectAmount = Object.values(profile.accounts['ID-1'].transactions);
 
-      for (const key in this.objectAmount) {
-        // if (this.objectAmount[key].type === 'from') {
-        //     this.objectAmount[key].amount = +this.objectAmount[key].amount * -1;
-        // }
+            for (const key in this.objectAmount) {
+                // if (this.objectAmount[key].type === 'from') {
+                //     this.objectAmount[key].amount = +this.objectAmount[key].amount * -1;
+                // }
 
-        total += +this.objectAmount[key].amount;
-      }
+                total += +this.objectAmount[key].amount;
+            }
 
-      this.amountCalculated = this.amountMY + total;
-      console.log(total);
-      console.log('Moje konta', this.accounts);
-      console.log('Moje kasa', this.objectAmount);
-    });
-  }
+            this.amountCalculated = this.amountMY + total;
+            console.log(total);
+            console.log('Moje konta', this.accounts);
+            console.log('Moje kasa', this.objectAmount);
+        });
+    }
 
-  addClass(change) {
-    change.active = !change.active;
-  }
-
+    addClass(change) {
+        change.active = !change.active;
+    }
+    showMore() {
+        this.end += 3;
+    }
 }
