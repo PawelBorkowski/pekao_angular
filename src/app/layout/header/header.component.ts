adb7928c46c77c4d7421d83aed612effe8925a98
PawelBorkowski <borekosp@gmail.com>
2020-02-08 22:40:16 +0100

    start working on menu mobile

--- src/app/layout/header/header.component.ts
+++ src/app/layout/header/header.component.ts
@@ -3,31 +3,36 @@ import { AuthService } from 'src/app/login/auth.service';
 import { Router } from '@angular/router';
 
 @Component({
-    selector: 'app-header',
-    templateUrl: './header.component.html',
-    styleUrls: ['./header.component.scss']
+  selector: 'app-header',
+  templateUrl: './header.component.html',
+  styleUrls: ['./header.component.scss']
 })
 export class HeaderComponent implements OnInit {
-    user: { surname: string; name: string };
-    constructor(private authService: AuthService, private router: Router) {}
+  user: { surname: string; name: string };
+  constructor(private authService: AuthService, private router: Router) { }
 
-    ngOnInit() {
-        this.authService.profile$.subscribe(profile => {
-            if (!profile) {
-                this.user = null;
-                return;
-            }
-            this.user = { surname: profile.profile.surname, name: profile.profile.name };
-            console.log(this.user);
-        });
-    }
+  ngOnInit() {
+    this.authService.profile$.subscribe(profile => {
+      if (!profile) {
+        this.user = null;
+        return;
+      }
+      this.user = { surname: profile.profile.surname, name: profile.profile.name };
+      console.log(this.user);
+    });
+  }
 
-    logout() {
-        //this.authService.logOut();
-        this.authService.logOut().then(() => this.router.navigate(['']));
-    }
+  addClass(change2) {
+    change2.active = !change2.active;
+  }
 
-    login() {
-        // this.authService.logIn();
-    }
+  logout() {
+    //this.authService.logOut();
+    this.authService.logOut().then(() => this.router.navigate(['']));
+  }
+
+  login() {
+    // this.authService.logIn();
+  }
+
 }
